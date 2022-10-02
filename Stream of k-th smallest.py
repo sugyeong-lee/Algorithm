872c1a1f6e225958a235c0c1e4b4a693d75f7ebe
sugyeong-lee <waterscape2021@naver.com>
2022-10-03 01:26:35 +0900

    22/10/02

--- Stream of k-th smallest.py
+++ Stream of k-th smallest.py
@@ -1,29 +1,11 @@
-s = 0
+import heapq
+
 A=[]
 A =list(map(int, input().split()))
-	
-def quick_select(A, k):   
-	p = A[0]
-	S, L, M =[], [], []
-	
-	for i in range(len(A)):
-		if A[i] < p: 
-			S.append(A[i])
-		elif A[i] > p: 
-			L.append(A[i])
-		else: 
-			M.append(A[i])
-			
-	if len(S) >= k:
-		return quick_select(S,k)
-	elif len(S) + len(M) < k:
-		return quick_select(L,k-len(S)-len(M))
-	else:
-		return p
-	
-for i in range(len(A)):
+
+i = 0
+m=[]
+while i <= len(A)-1:
+	m.append(A[i])
 	k = i//3+1
-	a = quick_select(A, k)
-	s += a
-	
-print(s)
+	print(m)
